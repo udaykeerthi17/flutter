7496e8e1d30581cbb6b13e655fb5a45c8e267167
udaykeerthi17 <147966208+udaykeerthi17@users.noreply.github.com>
2025-03-01 10:44:55 +0530

    Add files via upload

---
+++ flutter programs/array.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+bool isSubsetSum(List<int> arr, int sum) {
+  int n = arr.length;
+  List<List<bool>> dp = List.generate(n + 1, (_) => List.filled(sum + 1, false));
+
+  for (int i = 0; i <= n; i++) {
+    dp[i][0] = true;
+  }
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= sum; j++) {
+      if (arr[i - 1] <= j) {
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+
+  return dp[n][sum];
+}
+
+void main() {
+  print("Enter the array elements separated by spaces:");
+  String? arrayInput = stdin.readLineSync();
+  List<int> arr = arrayInput?.split(' ').map((e) => int.parse(e)).toList() ?? [];
+
+  print("Enter the target sum:");
+  String? sumInput = stdin.readLineSync();
+  int sum = int.tryParse(sumInput ?? '') ?? 0;
+
+  bool result = isSubsetSum(arr, sum);
+  print(result ? "True" : "False");
+}
---
+++ flutter programs/sorting array.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void sortOddAndEvenNumbers(List<int> numbers) {
+  List<int> oddNumbers = [];
+  List<int> evenNumbers = [];
+
+  for (int num in numbers) {
+    if (num.isOdd) {
+      oddNumbers.add(num);
+    } else {
+      evenNumbers.add(num);
+    }
+  }
+
+  oddNumbers.sort();
+  evenNumbers.sort((a, b) => b.compareTo(a));
+
+  int oddIndex = 0, evenIndex = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i].isOdd) {
+      numbers[i] = oddNumbers[oddIndex++];
+    } else {
+      numbers[i] = evenNumbers[evenIndex++];
+    }
+  }
+}
+
+void main() {
+  print("Please enter the numbers separated by spaces:");
+
+  String? input = stdin.readLineSync();
+  List<int> numbers = input?.split(' ').map((e) => int.parse(e)).toList() ?? [];
+
+  sortOddAndEvenNumbers(numbers);
+
+  print(numbers.join(' '));
+}
